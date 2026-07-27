844abcc10091e1d75ff81eaefe8e92000f7f96e8
Km4rv1n <marvinkika25@gmail.com>
2026-07-27 20:47:11 +0200

    Added session 5 exercise

---
+++ Session5_spring-basics-from-scratch/src/main/java/com/example/springbasics/NotificationManager.java
@@ -0,0 +1,17 @@
+package com.example.springbasics;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationManager {
+    private final NotificationService notificationService;
+
+    public NotificationManager(@Qualifier("smsNotificationService") NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void notifyUser(String message){
+        notificationService.send(message);
+    }
+}
---
+++ Session5_spring-basics-from-scratch/src/main/java/com/example/springbasics/NotificationService.java
@@ -0,0 +1,5 @@
+package com.example.springbasics;
+
+interface NotificationService {
+    void send(String message);
+}
